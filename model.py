a3b4d2524cdea1f5c2483dd3b753e12f225cfe60
Mohamed Lamhaouri <79314580+lamhaourii@users.noreply.github.com>
2026-09-15 17:53:50 +0100

    Step 20: assemble_feature_matrix

--- model.py
+++ model.py
@@ -181,8 +181,16 @@ def prepare_cleaned_features(X, iqr_k=1.5):
     X=clip_columns(X,lower,upper)
     return X
 
-# Step 20 - assemble_feature_matrix (not yet solved)
-# TODO: implement
+# Step 20 - assemble_feature_matrix
+def assemble_feature_matrix(X_num, ratio_num_idx, ratio_den_idx, cat_labels=None):
+    # TODO: build an extended feature matrix by appending a derived ratio...
+    ratio=make_ratio_feature(X_num[:,ratio_num_idx],X_num[:,ratio_den_idx])
+    X_num=append_column(X_num,ratio)
+    if cat_labels is not None:
+        enc=one_hot_encode(cat_labels)
+
+        X_num=np.hstack([X_num,enc])
+    return X_num
 
 # Step 21 - make_train_val_test (not yet solved)
 # TODO: implement
